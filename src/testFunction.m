c794557196b93758d2024d53527b9dd6a7a5a2bb
platte <rbp@asu.edu>
2012-10-12 10:39:52 -0700

    added a shift variable to the test function to avoid zero derivatives in v_x and u_y

--- src/testFunction.m
+++ src/testFunction.m
@@ -1,6 +1,9 @@
 function [tX, tY, DxtX, DxtY, DytX, DytY] = testFunction(X, Y, k1, k2)
 % Evaluate the test function and it's derivatives
 
+sx = 0.1; sy = 0.1; % Shift in X and Y
+X = X+sx; Y = Y+sy; % Shifted variables
+
 tX = +1/k1 * sin(k1*X) .* cos(k2*Y);
 tY = -1/k2 * cos(k1*X) .* sin(k2*Y);
 DxtX = +cos(k1*X) .* cos(k2*Y);
