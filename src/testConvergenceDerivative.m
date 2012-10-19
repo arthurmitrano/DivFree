cd809f5a472af822d2f49aadf7fb4bd9a3d108e7
Arthur Mitrano <amitrano@asu.edu>
2012-10-19 02:25:55 -0700

    After some tests, we need new ideas.
    I add a comparison with the regular FD method of 4th order (centered
    5-point stencil). Calculating the divergent at the origin we get
    something of the order of 10^-7, while using divFreeFD we get 10^-11.
    
    What would be really great is to get the 4th order decay without adding
    points to the stencil. I'm not sure, but maybe polynomial interpolation
    can be useful to introduce this extra interpolation points.

--- src/testConvergenceDerivative.m
+++ src/testConvergenceDerivative.m
@@ -8,10 +8,11 @@ m = 3;           % Degree of the univariate polynomial
 numPts = 3;      % The main stecil will have numPts^2 points
 
 
-uxErr = []; uyErr = []; vxErr = []; vyErr = [];  % Derivative errors
+uxErr = []; uyErr = []; vxErr = []; vyErr = []; % Derivative errors
+uxErrFD = []; uyErrFD = []; vxErrFD = []; vyErrFD = []; % Trad diff errors
 
-nn = 7:10:100; % The code only works for n odd, will get non-integer index
-               % otherwise
+nn = 11:10:200; % The code only works for n odd, will get non-integer index
+                % otherwise
 for n = nn
     tic
     xx = linspace(-1,1,n);
@@ -30,11 +31,13 @@ for n = nn
     % for now that is what we want to focus on. Later, we will generalize
     % this code for different stencil position.
     
-    yPts = gridCenterY-2:gridCenterY+2;
-    xPts = gridCenterX-2:gridCenterX+2;
-    uInterpPts = [xx(gridCenterX)*ones(size((-2:2)')) yy(yPts)']; % Extra interpolation points for component U
+    % Extra interpolation points for component U
+    yPts = [gridCenterY-2 gridCenterY+2]; % Add 2 points to the stencil
+    uInterpPts = [xx(gridCenterX)*[1; 1] yy(yPts)']; 
     %uInterpPts = [];
-    vInterpPts = [xx(gridCenterX)*ones(size((-2:2)')) yy(yPts)']; % Extra interpolation points for component V
+    % Extra interpolation points for component V
+    xPts = [gridCenterX-2 gridCenterX+2]; % Add 2 points to the stencil
+    vInterpPts = [xx(xPts)' yy(gridCenterY)*[1; 1]]; 
     %vInterpPts = [];
     
     [M, t1, t2, ux, uy, vx, vy] = FD_DivFreeMatrix(N, m, numPts, ...
@@ -46,10 +49,11 @@ for n = nn
     idx = (-(numPts-1)/2:(numPts-1)/2); % stencil on x (or y) direction
     I = gridCenterX + idx; % This will change on the future. Using 
     J = gridCenterY + idx; % length to get a stencil center at 0
-    
     U = u(I,J); V = v(I,J);  % Selects the points of the stencil
+    % Adding extra interpolation conditions
     U = U(:); V = V(:);
-    U = [U; u(yPts,gridCenterX)]; V = [V; v(yPts,gridCenterX) ];
+    U = [U; u(yPts,gridCenterX)]; V = [V; v(gridCenterY,xPts)' ];
+    
     coeff = Minv*[U; V; zeros([numPts^2,1])];  % Get poly coeffs
     
     % Numerical derivatives (ineficient code, will replace when we decide 
@@ -61,23 +65,39 @@ for n = nn
     vyAtO = vy(0,0)*coeff(numUnknows/2 + 1:end);
     uAtO = t1(0,0)*coeff(1:numUnknows/2);
     vAtO = t2(0,0)*coeff(numUnknows/2 + 1:end);
-    % Mesuring the error:
+    
+    % Measuring the error for divFree method:
     uxErr = [uxErr; abs(uxAtO - uxAtO_Exact)];
     uyErr = [uyErr; abs(uyAtO - uyAtO_Exact)];
     vxErr = [vxErr; abs(vxAtO - vxAtO_Exact)];
     vyErr = [vyErr; abs(vyAtO - vyAtO_Exact)];
+    
+    % Measuring the error for traditional finite differences
+    D4 = [1 -8 0 8 -1]/(12*h);  % 4th order 1st derivative weights
+    xPts = (gridCenterY-2:gridCenterY+2);
+    yPts = (gridCenterX-2:gridCenterX+2);
+    uxErrFD = [uxErrFD; abs(D4*u(gridCenterY,xPts)' - uxAtO_Exact)];
+    uyErrFD = [uyErrFD; abs(D4*u(yPts,gridCenterX) - uyAtO_Exact)];
+    vxErrFD = [vxErrFD; abs(D4*v(gridCenterY,xPts)' - vxAtO_Exact)];
+    vyErrFD = [vyErrFD; abs(D4*v(yPts,gridCenterX) - vyAtO_Exact)];
+    
     toc
+    
     n
 end
 
 figure(1)
-loglog(nn,uxErr,'r.-', nn,uyErr,'b.-', nn,nn.^-2,'b--', nn,nn.^-4,'r--')
-legend('uxErr','uyErr','N^{-2}','N^{-4}','Location','Best')
+loglog(nn,uxErr,'r.-', nn,uyErr,'b.-', nn,uxErrFD,'rs-', ...
+       nn,uyErrFD,'bs-', nn,nn.^-2,'b--', nn,nn.^-4,'r--')
+legend('uxErr','uyErr','uxErrFD','uyErrFD','N^{-2}','N^{-4}', ...
+       'Location','Best')
 title('Error on u derivatives')
 xlabel('N'), ylabel('Error')
 
 figure(2)
-loglog(nn,vxErr,'r.-', nn,vyErr,'b.-', nn,nn.^-2,'r--', nn,nn.^-4,'b--')
-legend('vxErr','vyErr','N^{-2}','N^{-4}','Location','Best')
+loglog(nn,vxErr,'r.-', nn,vyErr,'b.-', nn,vxErrFD,'rs-', ...
+       nn,vyErrFD,'bs-', nn,nn.^-2,'r--', nn,nn.^-4,'b--')
+legend('vxErr','vyErr','vxErrFD','vyErrFD','N^{-2}','N^{-4}', ...
+       'Location','Best')
 title('Error on v derivatives')
 xlabel('N'), ylabel('Error')
