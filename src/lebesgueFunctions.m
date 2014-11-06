41c7cb6997494b3ffbb76a40df94591baff9c228
arthurmitrano <amitrano@asu.edu>
2014-11-06 16:10:40 -0700

    Faster lebesgueFunctions (polynomial case)
    Remove the "local" part of the Lebesgue function for the polynomial
    case. Also, improved the speed of the function, since we are now solving
    only one linear system, instead of one for each cardinal function.

--- src/lebesgueFunctions.m
+++ src/lebesgueFunctions.m
@@ -1,88 +1,78 @@
 %% Lebesgue functions of divergence-free polynomial interpolants
 % The purpose of this script is to visualize the cardinal and Lebesgue
-% functions of the *local and global polynomial* divergence-free method
-% based on the *stream function* idea. The user can change the
-% interpolation points on the files <../uInterpPts.txt |uInterpPts.txt|>
-% and <../vInterpPts.txt |vInterpPts.txt|> (for the local case only), as
-% well as the degree $N$ of the polynomial
-% interpolant $(u_{interp},v_{interp})$. For more information on how this
+% functions of the *polynomial* divergence-free method based on the *stream
+% function* idea. The user can change the degree $N$ of the polynomial
+% interpolant $(u_{interp}, v_{interp})$. For more information on how this
 % polynomial is constructed see <FD_DivFreeMatrixStream.html
 % FD_divFreeMatrixStream.html>.
 
 %%
-function lebesgueConst = lebesgueFunctions(n,N,numPts,display,alpha)
-% Calculates Lebesgue function and it's constant. If numPts is not
-% provided, numPts = n. If numPts present then display = true else false.
+function lebesgueConst = lebesgueFunctions(n,N,display,alpha)
+%  Calculates Lebesgue function and it's constant.
 %
-% n       : Number of grid points in one dimension
-% N       : Degree of the bivariate interpolation polynomial
-% numPts  : The main stecil will have numPts^2 points (optional)
-% display : Display the cardinal function plots (optinal)
-% alpha   : Kosloff & Tal-Ezer parameter (default = 1)
+%  n       : Number of grid points in one dimension
+%  N       : Degree of the bivariate interpolation polynomial
+%  display : Display the cardinal function plots (optinal)
+%  alpha   : Kosloff & Tal-Ezer parameter (default = 1)
 %
-% NOTE: n and numPts must be odd numbers.
+%  NOTE: n must be odd numbers.
 
-%% Setting up the function:
+%% Setting up the function
 if (nargin <= 2)
-    numPts = n;             % Using all the grid points to interpolate
-    local = false;
     display = false;
-else
-    local = (n ~= numPts);  % The main stecil will have numPts^2 points
 end
-if (nargin == 3)
-    display = true;
-end
-if (nargin <= 4)
+
+if (nargin < 4)
     alpha = 1;
 end
 
-%% Generating 2d-grid of n^2 pts equally-spaced
-xx = linspace(-1,1,n);
+%% Generating 2d-grid of n^2 pts equally-spaced or mapped via K&T
+xCheb = sort(cos(pi*(0:n-1)/(n-1)));
+if (alpha ~= 0)
+    xx = asin(alpha*xCheb)/asin(alpha);
+else
+    xx = xCheb;
+end
 h = abs(xx(2) - xx(1));
+[X,Y] = meshgrid(xx);
+dSites = [X(:), Y(:)];
 
 hh = h/7;  % Spacement of the refined grid
-if local
-    xxRefined = -floor(numPts/2)*h:hh:floor(numPts/2)*h;
-else
-    xxRefined = -1:hh:1;
-end
+xxRefined = -1:hh:1;
 [XX, YY] = meshgrid(xxRefined); % Equal refinement on both directions
 
-%% Selecting the interpolation points
-% If local, we center the stencil at the origin. To choose specific points
-% of interpolations use the files <../uInterpPts.txt |uInterpPts.txt|>
-% and <../vInterpPts.txt |vInterpPts.txt|>. Make sure that the matrix is on
-% the correct dimension for a particular numPts _(numPts x numPts)_.
-if local
-    uIdx = find(load('uInterpPts.txt'));
-    vIdx = find(load('vInterpPts.txt')); % using linear indexes
-else % global, use all points of the grid
-    uIdx = find(ones(numPts));
-    vIdx = find(ones(numPts));
-end
-interpPts = struct('u', uIdx,'v', vIdx, 'numPts',numPts);
-
 %% Calculating the cardinal functions
-[M, uInterp, vInterp] = FD_DivFreeMatrixStream(h,N,interpPts,alpha);
+[M, uInterp, vInterp] = FD_DivFreeMatrixStream(dSites, N);
 
-L = zeros(numPts,2*numPts);  % used to construct delta function
-coeffs = zeros(size(M,2), length(uIdx) + length(vIdx));
+L = zeros(n,2*n);  % used to construct delta function
+
+% coeffs = zeros(size(M,2), length(uIdx) + length(vIdx));
     % lines  : number of columns of the divFree matrix M
     % columns: total amount of interpolation points for u and v
 
 lebesgueFunctionU = zeros(size(XX));
 lebesgueFunctionV = zeros(size(XX));
-for p = [uIdx; vIdx+numPts^2]' % 1:2*numPts^2 minus some pts.
+
+rhs = zeros(2*n^2);
+for p = 1:2*n^2 % all interpolation points
     L(p) = 1;
-    U = L(:,1:numPts);
-    V = L(:,numPts+1:2*numPts);
+    U = L(:,1:n);
+    V = L(:,n+1:2*n);
+    rhs(:,p) = [U(:); V(:)];
     L(p) = 0; % going back to zero matrix
-    coeffs(:,p) = M\[U(uIdx); V(vIdx)];  % WHY DO I KEEP THOSE COEFFS
-    [i,j] = ind2sub([numPts, numPts], rem(p-1,numPts^2)+1); 
-                                   % p = 1, ..., numPts^2, 1, ..., numPts^2
+end
+
+coeffs = M\rhs;
+
+for p = 1:2*n^2 % 1:2*numPts^2 minus some pts.
     cardFunctionU = uInterp(XX,YY,coeffs(:,p));
     cardFunctionV = vInterp(XX,YY,coeffs(:,p));
+
+    lebesgueFunctionU = lebesgueFunctionU + abs(cardFunctionU);
+    lebesgueFunctionV = lebesgueFunctionV + abs(cardFunctionV);
+
+    [i,j] = ind2sub([n, n], rem(p-1,n^2) + 1);
+    % p = 1, ..., numPts^2, 1, ..., numPts^2
     if display
         figure(1)
         set(gcf, 'Position', [100,100, 600*2, 600])
@@ -102,18 +92,15 @@ for p = [uIdx; vIdx+numPts^2]' % 1:2*numPts^2 minus some pts.
 
         set([h1 h2], 'clim', [-.3 1])
         snapnow
-        pause
+        pause(0.01)
     end
-
-    lebesgueFunctionU = lebesgueFunctionU + abs(cardFunctionU);
-    lebesgueFunctionV = lebesgueFunctionV + abs(cardFunctionV);
 end
 
 %%
-% Observe on the plots (case |n = 51|, |N = 3| and |numPts = 3|) that the
-% cardinal functions do not satisfy the interpolation condition exactly.
-% This happens because we are looking for the least-squares solution when
-% we use the backslash operator |"\"|.
+% Observe on the plots (case |n = 3| and |N = 3|) that the cardinal
+% functions do not satisfy the interpolation condition exactly. This
+% happens because we are looking for the least-squares solution when we use
+% the backslash operator |"\"|.
 %
 % Here we are using the interpolants based on the stream function, then, by
 % construction, they form a divergence-free vector field. However, as
@@ -145,4 +132,5 @@ if display
     title(['Lebesgue Function V for (i,j) = (' num2str(i) ',' num2str(j)...
         '), \Lambda_V = ' num2str(lebesgueConstV)])
 end
+
 end
