e6f3221e974c65a7718b0cea86921f3976f88c8c
arthurmitrano <amitrano@asu.edu>
2014-11-06 16:01:17 -0700

    Faster lebesgueFunctionRBF
    Now we solve just one linear system, instead of one for each cardinal
    function.

--- src/lebesgueFunctionsRBF.m
+++ src/lebesgueFunctionsRBF.m
@@ -71,6 +71,7 @@ L = zeros(n,2*n);  % used to construct delta function
 lebesgueFunctionU = zeros(size(XX));
 lebesgueFunctionV = zeros(size(XX));
 
+rhs = zeros(2*n^2);
 for p = 1:2*n^2  % all interpolation points
     L(p) = 1;
     U = L(:,1:n);
@@ -78,46 +79,49 @@ for p = 1:2*n^2  % all interpolation points
     t =  [U(:) V(:)];  % vector field
     L(p) = 0;          % going back to zero matrix
 
-    d = reshape(t', 1, numel(t))';
-    coeffs = A\d;
-    coeffs = reshape(coeffs, 2, size(t,1))';  % I'm not keeping the coeffs
+    rhs(:,p) = reshape(t', 1, numel(t))';
+
+end
+
+coeffs = A\rhs;
+
+for p = 1:2*n^2  % all interpolation points
+    c = reshape(coeffs(:,p), 2, size(t,1))';  % I'm not keeping the c's
     
-    cardFunction = RBFdivFreeInterp(coeffs, r_ePoints, d1_ePoints, ...
+    cardFunction = RBFdivFreeInterp(c, r_ePoints, d1_ePoints, ...
                                     d2_ePoints, F, G, ep);
     cardFunctionU = reshape(cardFunction(:,1), size(XX));
     cardFunctionV = reshape(cardFunction(:,2), size(XX));
 
-    [i,j] = ind2sub([n, n], rem(p-1,n^2) + 1); 
+    lebesgueFunctionU = lebesgueFunctionU + abs(cardFunctionU);
+    lebesgueFunctionV = lebesgueFunctionV + abs(cardFunctionV);
+
+    [i,j] = ind2sub([n, n], rem(p-1,n^2) + 1);
     if display
         figure(1)
-%         set(gcf, 'Position', [100,100, 600*2, 600])
-%         h1 = subplot(1,2,1);
-%         mesh(XX,YY,cardFunctionU)
+        % set(gcf, 'Position', [100,100, 600*2, 600])
+        % h1 = subplot(1,2,1);
+        % mesh(XX,YY,cardFunctionU)
         normcard = sqrt(cardFunctionU.^2+cardFunctionV.^2);
         quiver(XX,YY,cardFunctionU,cardFunctionV,max(max(normcard)))
-        %axis tight
+        % axis tight
         axis([-1 1 -1 1])
         hold on, plot(X,Y,'.k','markersize',20)
         plot(X(i,j),Y(i,j),'or','markersize',20), hold off
-%         axis([-1 1 -1 1 -.5 1])
         title(['Cardinal Function U for (i,j) = (' num2str(i) ',' ...
                num2str(j) ')'],'FontSize',15)
     
-%         h2 = subplot(1,2,2);
-%         mesh(XX,YY,cardFunctionV)
-%         axis([-1 1 -1 1 -.5 1])
-%         title(['Cardinal Function V for (i,j) = (' num2str(i) ',' ...
-%                num2str(j) ')'])
-%
-%         set([h1 h2], 'clim', [-.5 1])
+        % h2 = subplot(1,2,2);
+        % mesh(XX,YY,cardFunctionV)
+        % axis([-1 1 -1 1 -.5 1])
+        % title(['Cardinal Function V for (i,j) = (' num2str(i) ',' ...
+        %        num2str(j) ')'])
+        %
+        % set([h1 h2], 'clim', [-.5 1])
         snapnow
         pause
     end
-
-    lebesgueFunctionU = lebesgueFunctionU + abs(cardFunctionU);
-    lebesgueFunctionV = lebesgueFunctionV + abs(cardFunctionV);
 end
-
 %% 
 % Above we show the cardinal functions for the case where |n = 3|. Observe
 % that the divergence-free interpolant of the zero function is not zero,
